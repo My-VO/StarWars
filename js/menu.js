22331a05156da1193bd6616eeedc726574d78c21
My VO <kathy.nguyen198@gmail.com>
2021-02-01 21:54:07 +0100

    StarWars Mobile

---
+++ js/menu.js
@@ -0,0 +1,7 @@
+function openMenu() {
+    document.getElementById("menu-phone").style.width = "100vw";
+}
+  
+function closeMenu() {
+    document.getElementById("menu-phone").style.width = "0";
+  }
